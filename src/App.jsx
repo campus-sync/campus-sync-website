f57e690353ca92cbbe796b945111a2f0a827ded4
Hemanth N <jsshemanthn2003@gmail.com>
2024-10-25 17:06:27 +0530

    Added the Canteen Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import "./App.css";
 // import Test2 from "./components/test2";
 // import Header from "./components/header/header";
 import Vendor from "./components/vendor/vendor";
+import Canteen from "./components/canteen/canteen";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Vendor />
+      <Canteen />
     </>
   );
 }
