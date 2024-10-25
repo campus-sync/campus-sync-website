7f863e2e839fd063488029ed9ed7cf3726288e7e
Hemanth N <jsshemanthn2003@gmail.com>
2024-10-25 14:19:47 +0530

    Created the Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Test from "./components/test";
 import Test2 from "./components/test2";
+import Header from "./components/header/header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Test />
-      <Test2 />
+      <Header />
     </>
   );
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import styles from "./header.module.css";
+export default function Header() {
+  return (
+    <>
+      <div className={styles.header}>
+        <img
+          src="src\assets\CampusSync-Logo.png"
+          className={styles.header_logo}
+          alt="logo"
+        />
+        <div className={styles.header_title}>CampusSync</div>
+      </div>
+    </>
+  );
+}
