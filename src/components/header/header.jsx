242a5eb90240aa80e5b4560d5b74ab448ac157c0
HiteshIDK <10hiteshparmar@gmail.com>
2024-10-26 09:20:10 +0530

    added some commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./header.module.css";
-import { GetTokenCookie, GetUserCookie } from "../../util/auth/cookies";
+import {
+  DeleteTokenCookie,
+  DeleteUserCookie,
+  GetTokenCookie,
+  GetUserCookie,
+} from "../../util/auth/cookies";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/CampusSync-Logo.png";
+import SidebarMenu from "../menu/menu";
+import { DeleteUserLocalStorage } from "../../util/auth/local-storage";
 
 export default function Header() {
   const router = useNavigate();
+  const [links, setLinks] = useState([]);
+
+  const logoutHandler = () => {
+    DeleteUserCookie();
+    DeleteTokenCookie();
+    DeleteUserLocalStorage();
+    router("/");
+  };
 
   useEffect(() => {
     const user = GetUserCookie();
@@ -14,17 +29,45 @@ export default function Header() {
     if (!user || !accessToken) {
       return router("/");
     }
-  });
+
+    if (user.account_type === "admin") {
+      setLinks([
+        {
+          name: "Colleges",
+          url: "/colleges",
+        },
+      ]);
+    } else if (user.account_type === "student") {
+      setLinks([
+        {
+          name: "Calendar",
+          url: "/calendar",
+        },
+        {
+          name: "Canteen",
+          url: "/canteen",
+        },
+        {
+          name: "Vendors",
+          url: "/vendor",
+        },
+      ]);
+    }
+  }, [router]);
 
   return (
     <>
       <div className={styles.header}>
-        <div></div>
+        <div>
+          <SidebarMenu links={links} />
+        </div>
         <div className={styles.header_logo_title}>
           <img src={logo} className={styles.header_logo} alt="logo" />
           <div className={styles.header_title}>CampusSync</div>
         </div>
-        <button className={styles.logout}>Log Out</button>
+        <button className={styles.logout} onClick={logoutHandler}>
+          Log Out
+        </button>
       </div>
     </>
   );
